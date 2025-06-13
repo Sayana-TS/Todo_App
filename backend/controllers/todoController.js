8bd6fb38258e4fddddf673ce7161c0ccc65337a9
Sayana TS <sayanatsudheesh@gmail.com>
2025-06-13 15:51:28 +0530

    updated todo and used getTodoById

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,50 +1,77 @@
 import Todos from "../model/todoModel.js";
 
-const createTodo = async (req, res)=>{
-    try {
-        let {title, description} = req.body
+const createTodo = async (req, res) => {
+  try {
+    let { title, description } = req.body;
 
-        let newTodo = await Todos.create({
-            title,
-            description
-        })
+    let newTodo = await Todos.create({
+      title,
+      description,
+    });
 
-        res.json(newTodo)
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
+    res.json(newTodo);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getTodo = async (req, res) => {
-    try {
-        const todos = await Todos.find()
-        res.json(todos)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+  try {
+    const todos = await Todos.find();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
+const getTodoById = async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    const todo = await Todos.findById(id);
+
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found " });
+    }
+
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 const deleteTodo = async (req, res) => {
-    try {
-        const deleted = await Todos.findByIdAndDelete(req.params.id)
+  try {
+    const deleted = await Todos.findByIdAndDelete(req.params.id);
 
-        if(!deleted){
-            return res.status(404).json({ message : 'Todo not found '})
-        }
-
-        res.json({ message : 'Todo deleted successfully'})
-
-    } catch (error) {
-        res.status(500).json(error)
+    if (!deleted) {
+      return res.status(404).json({ message: "Todo not found " });
     }
-}
 
+    res.json({ message: "Todo deleted successfully" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-export {
-    createTodo,
-    getTodo,
-    deleteTodo
-}
+const updateTodo = async (req, res) => {
+  try {
+    const { title, description, isCompleted, id } = req.body;
+
+    const updatedTodo = await Todos.findByIdAndUpdate(
+      id,
+      { title, description, isCompleted },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      return res.status(404).json({ message: "Todo not found " });
+    }
+
+    res.json(updatedTodo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export { createTodo, getTodo, deleteTodo, updateTodo, getTodoById, };
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createTodo, deleteTodo, getTodo } from '../controllers/todoController.js'
+import { createTodo, deleteTodo, getTodo, getTodoById, updateTodo } from '../controllers/todoController.js'
 
 const todoRoute = express.Router()
 
 todoRoute.post('/create-todo', createTodo)
 todoRoute.get('/getTodos', getTodo)
 todoRoute.delete('/:id', deleteTodo)
+todoRoute.patch('/update', updateTodo)
+todoRoute.get('/getTodoById', getTodoById)
 
 export default todoRoute
