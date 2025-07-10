832b1e9847d756c88e908a71917d234884a94731
Sayana TS <sayanatsudheesh@gmail.com>
2025-07-10 15:35:28 +0530

    created login and register page.

--- Frontend/src/Screens/RegisterPage.jsx
+++ Frontend/src/Screens/RegisterPage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import OwlWelcomeImage from '../assets/owl-welcome-img.png'
+import './RegisterPage.css'
 
 function RegisterPage() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <div>RegisterPage</div>
+    <>
+      <div className="container-fluid screen" style={{ minHeight: '100vh' }}>
+        <div className="main-box m-auto p-5" style={{ height: 'fit-content' }}>
+          <div className="form-box text-light text-center position-relative">
+            <img src={OwlWelcomeImage} alt="Owl Welcoming" className='owl-img' />
+            <form>
+              <h2>New Here? Let’s Set You Up!</h2>
+              <div className="fields d-flex flex-column gap-3 pt-4">
+                <input type="text" placeholder='Enter Username' className='p-2 text-light' value={name} onChange={(e)=> setName(e.target.value)}/>
+                <input type='email' placeholder='Enter Email' className='p-2 text-light' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <input type='password' placeholder='Enter Password' className='p-2 text-light'value={password} onChange={(e)=> setPassword(e.target.value)} />
+                <button type='submit' className='p-2'>REGISTER</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
